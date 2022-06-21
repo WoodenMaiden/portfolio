9ff5d5f0f53e0ff9811348803be061e2cba522c6
WoodenMaiden <yann.pomie@etu.umontpellier.fr>
2022-06-21 23:42:27 +0200

    fix: typos and more text

--- src/commands/whoami.js
+++ src/commands/whoami.js
@@ -12,10 +12,10 @@ function whoami(input) {
                     <Typography>Yann POMIE</Typography>
                 </Stack>
                 <Typography variant="body1">
-                    My name is Yann POMIE, I am currently a {new Date(Date.now() - new Date('June 19, 2001').getTime()).getFullYear() - 1970} years old french DevOps student at <a href="https://www.polytech.umontpellier.fr/formation/cycle-ingenieur/devops/en-quelques-mots-do">Polytech Montpllier</a>. 
+                    My name is Yann POMIE, I am currently a {new Date(Date.now() - new Date('June 19, 2001').getTime()).getFullYear() - 1970} years old french DevOps apprentice at <a href="https://www.polytech.umontpellier.fr/formation/cycle-ingenieur/devops/en-quelques-mots-do">Polytech Montpllier</a>. 
                     Prior to this I studied IT at the <a href='https://iut.univ-amu.fr/diplomes/but-informatique-info-aix'>Aix-en-Provence's IUT</a> for two Years.
                     <br />
-                    I discovered computer science around 2017 during high school thanks to lua and a framework called <a href='https://love2d.org/'>LÖVE2D</a>, and neer stopped learning ever since!
+                    I discovered computer science around 2017 during high school thanks to lua and a framework called <a href='https://love2d.org/'>LÖVE2D</a>, and never stopped learning ever since! I am currently working as a DevOps engineer at the <a href='https://www.ird.fr/'>IRD</a>, a French national instutute that leads researches concerning sustainable development. I am working on their agronomic knowledge platform <a href='http://agrold.southgreen.fr/agrold/'>AgroLD</a>
                     <br />
                     Besides code and linux, I am a mostly a huge fan of video games such as League of Legends, Warframe or even the XCOM franchise. I also listen to a lot of heavy-metal and rock-n-roll, My favourite bands are <a href='https://www.youtube.com/watch?v=WmPTxrJ-kHI'>Iron Maiden</a>, <a href='https://www.youtube.com/watch?v=MlzTET_8SQg'>Motörhead</a>, <a href='https://www.youtube.com/watch?v=l482T0yNkeo'>ACDC</a> and <a href='https://www.youtube.com/watch?v=o1tj2zJ2Wvg'>Guns N' Roses</a>.
                     <br />
